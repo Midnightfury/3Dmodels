0aede8a27b4e62a1a1892855d85794d31a0a7410
Raghavendra Sinha <90714630+Midnightfury@users.noreply.github.com>
2021-12-24 16:50:03 +0530

    Update aframe_user_angular_lighting_1.js

--- htmlFiles/js/aframe_user_angular_lighting_1.js
+++ htmlFiles/js/aframe_user_angular_lighting_1.js
@@ -1,17 +1,15 @@
+let directionalLight = document.querySelector("#directional-light");
 let lightSlider = document.querySelector("#light-position");
+
 let angularValue = document.querySelector("#angle-value");
 
 angularValue.innerHTML = lightSlider.value + " deg";
 
-let xCoordinate, zCoordinate;
-
 lightSlider.oninput = function () {
   let DegtoRad = this.value * (Math.PI / 180);
 
-  xCoordinate = (2 * Math.cos(DegtoRad)).toFixed(4);
-  zCoordinate = (2 * Math.sin(DegtoRad)).toFixed(4);
+  directionalLight.object3D.position.x = (2 * Math.cos(DegtoRad)).toFixed(4);
+  directionalLight.object3D.position.z = (2 * Math.sin(DegtoRad)).toFixed(4);
 
   angularValue.innerHTML = this.value + " deg";
 };
-
-export { xCoordinate, zCoordinate };
